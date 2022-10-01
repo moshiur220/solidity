e94d88a7da3b9ca0899417feec48d16b614a9fcf
Moshiur Rahman <moshiur.rahman@ekkbaz.com>
2022-10-01 15:37:05 +0600

    simple variable change

---
+++ variableChange.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+/*
+To write or update a state variable you need to send a transaction.
+
+On the other hand, you can read state variables, for free, without any transaction fee.
+*/
+contract SimpleStorage {
+    // State variable to store a number
+    uint public num;
+
+    // You need to send a transaction to write to a state variable.
+    function set(uint _num) public {
+        num = _num;
+    }
+
+    // You can read from a state variable without sending a transaction.
+    function get() public view returns (uint) {
+        return num;
+    }
+}
